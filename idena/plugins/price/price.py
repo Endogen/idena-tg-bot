b8b60f8137570d256f3c5c689c5632ee67099f91
Endogen <081081@gmx.net>
2020-03-11 23:36:36 +0100

    Added link to https://idena.today for price command

--- idena/plugins/price/price.py
+++ idena/plugins/price/price.py
@@ -28,7 +28,7 @@ class Price(IdenaPlugin):
         for target, price in result["tickers"][0]["converted_last"].items():
             reply += f"{target.upper():<5}{price:.8f}\n"
 
-        cg_link = f"\n[👉 https://idena.today 👈](https://idena.today)"
+        cg_link = f"\n👉 https://idena.today 👈"
 
         update.message.reply_text(
             text=f"`{reply}`{cg_link}",
