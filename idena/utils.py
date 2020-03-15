2257fa6277d93a6d4ee8267e741ae98dc323dc45
Endogen <081081@gmx.net>
2020-03-15 01:28:30 +0100

    Added function to format amounts

--- idena/utils.py
+++ idena/utils.py
@@ -16,6 +16,81 @@ def is_numeric(string):
     return False
 
 
+def format(value,
+           decimals=None,
+           force_length=False,
+           template=None,
+           on_zero=0,
+           on_none=None,
+           symbol=None):
+    """Format a crypto coin value so that it isn't unnecessarily long"""
+
+    fiat = False
+    if symbol and isinstance(symbol, str):
+        if symbol.lower() in get_fiat_list():
+            fiat = True
+
+    if value is None:
+        return on_none
+
+    try:
+        if isinstance(value, str):
+            value = value.replace(",", "")
+        v = float(value)
+    except Exception:
+        return str(value)
+
+    try:
+        if isinstance(template, str):
+            template = template.replace(",", "")
+        t = float(template)
+    except Exception:
+        t = v
+
+    try:
+        decimals = int(decimals)
+    except Exception:
+        decimals = None
+
+    try:
+        if float(value) == 0:
+            return on_zero
+    except Exception:
+        return str(value)
+
+    if t < 1:
+        if decimals:
+            v = "{1:.{0}f}".format(decimals, v)
+        else:
+            v = "{0:.8f}".format(v)
+    elif t < 100:
+        if decimals:
+            v = "{1:.{0}f}".format(decimals, v)
+        else:
+            v = "{0:.4f}".format(v)
+    elif t < 10000:
+        if decimals:
+            v = "{1:,.{0}f}".format(decimals, v)
+        else:
+            v = "{0:,.2f}".format(v)
+    else:
+        v = "{0:,.0f}".format(v)
+
+    if not force_length:
+        cut_zeros = False
+
+        if t >= 1:
+            cut_zeros = True
+        else:
+            if fiat:
+                cut_zeros = True
+
+        if cut_zeros:
+            while "." in v and v.endswith(("0", ".")):
+                v = v[:-1]
+    return v
+
+
 def esc_md(text):
     import re
 
